1aa0178ad34bd98367387e6a6df8d908c9db1b71
Franco Moloche <francomolocheg99@gmail.com>
2025-04-07 14:45:53 -0500

    [ADD] Implement Programs page with program loading and date selection functionality

---
+++ src/pages/Programs/page.tsx
@@ -0,0 +1,58 @@
+import { useEffect, useMemo, useState } from "react";
+import { Skeleton } from "@/components/ui/skeleton";
+import ProgramsService from "./services/ProgramsService";
+import { Program } from "./types/Program";
+import ProgramDatePicker from "./components/ProgramDatePicker/ProgramDatePicker";
+import ProgramContainer from "./components/ProgramContainer/ProgramContainer";
+
+export default function Expositors() {
+  const [loading, setLoading] = useState(true);
+  const [error, setError] = useState<string | null>(null);
+  const [programs, setPrograms] = useState<Program[]>([]);
+  const [dates, setDates] = useState<string[]>([]);
+  const [selectedDate, setSelectedDate] = useState<string | null>(null);
+
+  useEffect(() => {
+    const loadPrograms = async () => {
+      try {
+        const programs = await ProgramsService.getPrograms();
+        setPrograms(programs);
+        const dates = programs.map((program) => program.fechaPrograma);
+        setDates(dates);
+        if (dates.length > 0) setSelectedDate(dates[0]);
+      } catch {
+        setError("Error al cargar programas");
+      } finally {
+        setLoading(false);
+      }
+    };
+    loadPrograms();
+  }, []);
+
+  const programsByDate = useMemo(() => {
+    if (!selectedDate) return [];
+    return programs.filter((program) => program.fechaPrograma === selectedDate);
+  }, [programs, selectedDate]);
+
+  if (error) return <p className="text-red-500">{error}</p>;
+
+  if (loading)
+    return (
+      <div className="grid grid-cols-1 gap-4">
+        <Skeleton className="h-6 w-full bg-primary/60" />
+        <Skeleton className="h-6 w-full bg-primary/60" />
+        <Skeleton className="h-6 w-full bg-primary/60" />
+      </div>
+    );
+
+  return (
+    <div>
+      <ProgramDatePicker
+        dates={dates}
+        selectedDate={selectedDate}
+        setSelectedDate={setSelectedDate}
+      />
+      <ProgramContainer programs={programsByDate} />
+    </div>
+  );
+}
