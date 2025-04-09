254d2a0bd8087248ff01d209c0da379a5ceb4dd2
Franco Moloche <extdev08@acity.com.pe>
2025-04-09 00:37:40 -0500

    [FIX] Update API endpoints for fetching programs and categories

--- src/pages/Programs/services/ProgramsService.tsx
+++ src/pages/Programs/services/ProgramsService.tsx
@@ -1,11 +1,12 @@
+import { ProgramForm } from "../components/ProgramForm/types/ProgramForm";
 import { Program, ProgramCategory } from "../types/Program";
 
-const BASE_URL = "https://ab7cuydlkd.execute-api.us-east-1.amazonaws.com";
+const BASE_URL = "https://jd3gnip3o3.execute-api.us-east-1.amazonaws.com";
 const BASE_URL2 = "https://ktgbqfdb72.execute-api.us-east-1.amazonaws.com";
 
 const ProgramsService = {
   getPrograms: async () => {
-    const res = await fetch(`${BASE_URL}/mob/program/event/1`);
+    const res = await fetch(`${BASE_URL}/web/program/event/1`);
     if (!res.ok) throw new Error("Error fetching programs");
     const data = await res.json();
     return data as Program[];
@@ -15,6 +16,13 @@ const ProgramsService = {
     if (!res.ok) throw new Error("Error fetching program categories");
     const data = await res.json();
     return data as ProgramCategory[];
+  },
+  addProgram: async (program: ProgramForm) => {
+    const res = await fetch(`${BASE_URL}/web/program/event`, {
+      method: "POST",
+      body: JSON.stringify([program]),
+    });
+    if (!res.ok) throw new Error("Error adding program");
   },
 };
 
